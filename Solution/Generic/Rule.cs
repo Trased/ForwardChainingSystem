88a17928802900c4cbebd37adea519d82c6a5443
Trased <117777557+Trased@users.noreply.github.com>
2024-12-24 17:17:16 +0200

    Inclusion Exclusion Principle implementation

---
+++ Solution/Generic/Rule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    public class Rule
+    {
+        public List<string> Premises { get; set; }
+        public string Conclusion { get; set; }
+
+        public Rule(List<string> premises, string conclusion)
+        {
+            Premises = premises;
+            Conclusion = conclusion;
+        }
+
+        public bool IsSatisfied(HashSet<string> knownFacts)
+        {
+            return Premises.TrueForAll(knownFacts.Contains);
+        }
+
+        public string EvaluateConclusion(Dictionary<string, int> numericalFacts)
+        {
+            string processedConclusion = Conclusion;
+
+            foreach (var fact in numericalFacts)
+            {
+                processedConclusion = processedConclusion.Replace($"|{fact.Key}|", fact.Value.ToString());
+            }
+
+            using (var table = new System.Data.DataTable())
+            {
+                return table.Compute(processedConclusion.Split('=')[1], "").ToString();
+            }
+        }
+    }
+
+}
---
+++ Solution/Principles/Abstract/IPrinciple.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Solution
+{
+    public interface IPrinciple
+    {
+        string Name { get; }
+        void AddPremise(string premise);
+        void AddRule(string rule);
+        List<string> Infer();
+    }
+}
